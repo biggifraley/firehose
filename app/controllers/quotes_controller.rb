bf6540807679c3b9b0333413af894d3a30464fce
Biggi Fraley <biggi.fraley@usa.net>
2015-09-25 00:14:00 +0000

    Perform form validations.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,18 @@ class QuotesController < ApplicationController
 	def new
 		@quote = Quotes.new
 	end
+
+	def create
+		@quote = Quotes.create(quote_params)
+		if @quote.invalid?
+			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+		end
+		redirect_to root_path
+	end
+
+	private
+
+	def quote_params
+		params.require(:quotes).permit(:saying, :author)
+	end
 end
